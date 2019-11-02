40da618965536de7bab1ec8c19e9913c50f5ec1d
nmkip <juan.ignacio848@gmail.com>
2019-11-02 19:40:49 -0300

    Adding h2 database

--- src/customers/handlers.clj
+++ src/customers/handlers.clj
@@ -1,6 +1,6 @@
 (ns customers.handlers
   (:require [customers.model :as model]
-    [ring.util.response :refer [response created]]))
+            [ring.util.response :refer [response created]]))
 
 (defn customer-id [req]
   (-> req
@@ -8,19 +8,18 @@
       :id))
 
 (defn create-customer [req]
-  (created "" (model/create-customer' (:datasource req) (:body req))))
+  (created "" (model/create-customer (:datasource req) (:body req))))
 
 (defn delete-customer [req]
-  (model/delete-customer (customer-id req)))
+  (model/delete-customer (:datasource req) (get-in req [:path-params :id])))
 
 (defn find-customer [req]
-  (-> req
-      customer-id
-      model/find-customer
-      response))
+  (response (model/find-customer (:datasource req) (get-in req [:path-params :id]))))
 
 (defn find-customers [req]
   (response {:customers (model/find-customers (:datasource req))}))
 
 (defn update-customer [req]
-  (response (model/update-customer (customer-id req) (:body req))))
+  (tap> {:body (:body req)})
+  (response (model/update-customer (:datasource req)
+                                   (assoc (:body req) :id (get-in req [:path-params :id])))))
--- src/customers/model.clj
+++ src/customers/model.clj
@@ -1,33 +1,22 @@
 (ns customers.model
-  (:require [next.jdbc :as jdbc]
-            [next.jdbc.sql :as sql])
-  (:import (java.util UUID)))
+  (:require [next.jdbc.sql :as sql]))
 
-(def customers (atom {}))
-
-(defn uuid []
-  (.toString (UUID/randomUUID)))
-
-(defn create-customer [data]
-  (let [uuid (uuid)
-        customer (merge {:id uuid} data)]
-    (swap! customers merge {uuid customer})
-    customer))
-
-(defn create-customer' [db customer]
+(defn create-customer [db customer]
   (assoc customer :id (:CUSTOMER/ID (sql/insert! db :customer customer))))
 
-(defn delete-customer [id]
-  (swap! customers dissoc id))
+(defn delete-customer [db id]
+  (sql/delete! db :customer {:id id}))
 
-(defn find-customer [id]
-  (@customers id))
+(defn find-customer [db id]
+  (sql/get-by-id db :customer id))
 
-(defn find-customers [datasource]
-  (tap> {:datasource (type datasource)})
-  (or (vals @customers) []))
+(defn find-customers [db]
+  (sql/query db ["select * from customer"]))
 
-(defn update-customer [id data]
-  (let [data (dissoc data :id)]
-    (swap! customers assoc id (merge (@customers id) data))
-    (@customers id)))
+(defn update-customer [db customer]
+  (sql/update! db
+               :customer
+               {:first_name (:first_name customer)
+                :last_name  (:last_name customer)}
+               {:id (:id customer)})
+  customer)
